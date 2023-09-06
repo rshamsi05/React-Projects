d8f9ec80af299b8cccdc98d40882f284a9789eb3
rshamsi05 <shamsirayan16@gmail.com>
2023-09-05 22:16:29 -0400

    Reworked some of the transaction displays

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -5,9 +5,11 @@ import Balance from './Components/Balance';
 import IncomeExpenses from './Components/IncomeExpenses';
 import TransactionList from './Components/TransactionList';
 import AddTransaction from './Components/AddTransaction';
+
+import { GlobalProvider } from './Context/GlobalState';
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- expenses/src/Components/TransactionList.js
+++ expenses/src/Components/TransactionList.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState';
 
 const styles = {color: 'White'};
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+
   return (
     <>
       <h3 style={styles}>History</h3>
       <ul className="list">
-        <li style={styles} className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li>
+        {transactions.map(transaction => (<li style={styles} className="minus">
+          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+        </li>))}
       </ul>
     </>
   )
